3a053d60423601b3596543b8c5cf037648b7c5fc
Igor Santos <igr.exe@gmail.com>
2018-09-17 18:30:49 -0300

    feat(server): add parse to oficial spreadsheet structure

--- server/index.js
+++ server/index.js
@@ -2,49 +2,67 @@ import { google } from 'googleapis';
 import assert from 'assert';
 import express from 'express';
 
+// Assert required enviroment variables for app
 assert(process.env.GCLOUD_PROJECT !== undefined, 'Required enviroment variable GCLOUD_PROJECT');
 assert(process.env.GOOGLE_APPLICATION_CREDENTIALS !== undefined, 'Required enviroment GOOGLE_APPLICATION_CREDENTIALS');
 assert(process.env.GOOGLE_SPREADSHEET_ID !== undefined, 'Required enviroment GOOGLE_SPREADSHEET_ID');
 assert(process.env.PORT !== undefined, 'Required enviroment PORT');
 
 const asyncMiddleware = fn => (req, res, next) => {
-    Promise.resolve(fn(req, res, next))
-        .catch(next);
+  Promise.resolve(fn(req, res, next))
+    .catch(next);
 };
 
-async function main (req, res, next) {
-    const auth = await google.auth.getClient({
-        scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
-    });
+const parseSpreadsheet = (rows) => {
+  const rowKeys = {
+    'first_name': 3,
+    'last_name': 4,
+    'state': 0,
+    'city': 1,
+    'expertness': 8
+  };
 
-    // const project = await google.auth.getDefaultProjectId();
-    const sheets = google.sheets('v4');
-    sheets.spreadsheets.values.get({
-        auth: auth,
-        spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
-        range: 'A2:E'
-    }, (err, sheetRes) => {
-        if (err) {
-            console.error('The API returned an error.');
-            throw err;
-        }
-        const rows = sheetRes.data.values;
-        if (rows.length === 0) {
-            console.log('No data found.');
-        } else {
-            console.log('Leitura da planilha realizada com sucesso!\n');
-            const jsonResponse = [];
-
-            for (const row of rows) {
-                // Print columns A and E, which correspond to indices 0 and 4.
-                jsonResponse.push({
-                    name: row[0],
-                    major: row[4]
-                });
-            }
-            res.json(jsonResponse);
-        }
+  return rows.map(row => {
+    const json = {};
+    Object.keys(rowKeys).forEach(keyName => {
+      json[keyName] = row[rowKeys[keyName]];
     });
+    return json;
+  });
+};
+
+const main = async (req, res, next) => {
+  // Use GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS to authenticate
+  // service-to-service on googleapis 
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  });
+
+  // Get values of GOOGLE_SPREADSHEET_ID
+  // you need to give permission for your service account
+  const sheets = google.sheets('v4');
+  sheets.spreadsheets.values.get({
+    auth: auth,
+    spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
+    range: 'A2:P'
+  }, (err, sheetRes) => {
+    if (err) {
+      console.error('The API returned an error.');
+      throw err;
+    }
+
+    console.log('The API returned with success!\n')
+    const rows = sheetRes.data.values;
+    if (rows.length === 0) {
+      console.log('No data found.');
+      return res.json([])
+    } else {
+      console.log('Parse spreadsheet start...\n');
+      const jsonResponse = parseSpreadsheet(rows);
+      console.log('Parse spreadsheet is done!\n')
+      return res.json(jsonResponse);
+    }
+  });
 }
 
 const app = express();
@@ -55,6 +73,6 @@ app.get('/', asyncMiddleware(main));
 app.listen(port, () => console.log(`Match Voluntarios App listening on port ${port}!`));
 
 app.use(function(err, req, res, next) {
-    console.error(err);
-    res.status(500).json({message: 'an error occurred'});
+  console.error(err);
+  res.status(500).json({message: 'an error occurred'});
 });
