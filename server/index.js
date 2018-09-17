585eff83ada8a0bdf3265fe4a1b7da8a1a18630a
Lucas Pirola <lucaspirola@gmail.com>
2018-09-17 16:59:05 -0300

    feature(server): Add expressjs endpoint to show spreadsheet results

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import { google } from 'googleapis';
 import assert from 'assert';
+import express from 'express';
 
 assert(process.env.GCLOUD_PROJECT !== undefined, 'Required enviroment variable GCLOUD_PROJECT');
 assert(process.env.GOOGLE_APPLICATION_CREDENTIALS !== undefined, 'Required enviroment GOOGLE_APPLICATION_CREDENTIALS');
 assert(process.env.GOOGLE_SPREADSHEET_ID !== undefined, 'Required enviroment GOOGLE_SPREADSHEET_ID');
+assert(process.env.PORT !== undefined, 'Required enviroment PORT');
 
-async function main () {
+
+const asyncMiddleware = fn =>
+      (req, res, next) => {
+          Promise.resolve(fn(req, res, next))
+              .catch(next);
+      };
+
+async function main (req, res, next) {
 
     // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
     // environment variables.
@@ -21,20 +30,27 @@ async function main () {
         auth: auth,
         spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
         range: 'A2:E'
-    }, (err, res) => {
+    }, (err, sheetRes) => {
         if (err) {
             console.error('The API returned an error.');
             throw err;
         }
-        const rows = res.data.values;
+        const rows = sheetRes.data.values;
         if (rows.length === 0) {
             console.log('No data found.');
         } else {
-            console.log('Name, Major:');
+            console.log('Leitura da planilha realizada com sucesso!\n');
+            const jsonResponse = [];
+
             for (const row of rows) {
                 // Print columns A and E, which correspond to indices 0 and 4.
-                console.log(`${row[0]}, ${row[4]}`);
+                // console.log(`${row[0]}, ${row[4]}`);
+                jsonResponse.push({
+                    name: row[0],
+                    major: row[4]
+                });
             }
+            res.json(jsonResponse);
         }
     });
     // console.log(sheets);
@@ -44,4 +60,14 @@ async function main () {
     // console.log(res.data);
 }
 
-main().catch(console.error);
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', asyncMiddleware(main));
+
+app.listen(port, () => console.log(`Match Voluntarios App listening on port ${port}!`));
+
+app.use(function(err, req, res, next) {
+    console.error(err);
+    res.status(500).json({message: 'an error occurred'});
+});
