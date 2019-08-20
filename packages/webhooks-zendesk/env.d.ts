9a5f8a0a7d34e1b8ab89bfc0460c8b55b3284ad5
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-08-20 17:48:14 -0300

    feat(zendesk): add some env vars

--- packages/webhooks-zendesk/env.d.ts
+++ packages/webhooks-zendesk/env.d.ts
@@ -2,6 +2,9 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     ZENDESK_API_URL: string
     ZENDESK_API_TOKEN: string
+    ZENDESK_API_USER: string
+    ZENDESK_ORGANIZATIONS: string
+    GOOGLE_MAPS_API_KEY: string
     PORT: string
     FORM_NAME: string
   }
