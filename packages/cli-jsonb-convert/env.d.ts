87e024bf6db7b6fe3e705f4ad2387941635edc0c
Gabriel Rocha de Oliveira <rolivegab@localhost.localdomain>
2019-09-02 11:54:13 -0300

    chore: remove cli-jsonb-convert

--- packages/cli-jsonb-convert/env.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    DEBUG: string
-    HASURA_API_URL: string
-    X_HASURA_ADMIN_SECRET: string
-  }
-}
--- packages/cli-jsonb-convert/src/index.ts
+++
@@ -1,6 +0,0 @@
-import Server from './Server'
-import dotenv from 'dotenv'
-
-dotenv.config()
-const app = new Server()
-app.start()
