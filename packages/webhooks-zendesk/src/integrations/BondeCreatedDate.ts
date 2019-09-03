9647aff09aa1ba9d8b27509adaa83a7ff5ee2e76
Gabriel Rocha de Oliveira <rolivegab@localhost.localdomain>
2019-09-03 17:24:11 -0300

    feat(zendesk): implement bondeCreatedDate

---
+++ packages/webhooks-zendesk/src/integrations/BondeCreatedDate.ts
@@ -0,0 +1,88 @@
+import axios from 'axios'
+
+const query = `{
+  form_entries {
+    fields
+    created_at
+    widget_id
+  }
+}`
+
+interface FormEntry {
+  fields: [{}, {}, {value: string}]
+  created_at: string
+  widget_id: number
+}
+
+interface DataType {
+  data: {
+    form_entries: FormEntry[]
+  }
+}
+
+class BondeCreatedDate {
+  email: string
+  constructor(email: string) {
+    this.email = email
+  }
+
+  getFormEntries = async () => {
+    const {HASURA_API_URL, X_HASURA_ADMIN_SECRET} = process.env
+    const {data: {data: { form_entries }}} = await axios.post<DataType>(HASURA_API_URL!, {
+      query
+    }, {
+      headers: {
+        'x-hasura-admin-secret': X_HASURA_ADMIN_SECRET
+      }
+    })
+    
+    return form_entries
+  }
+
+  filterByWidgetId = (formEntries: FormEntry[]) => {
+    const {WIDGET_IDS} = process.env
+    try {
+      const widgets = JSON.parse(WIDGET_IDS)
+      const advogadaId = widgets['ADVOGADA']
+      const psicologaId = widgets['PSICÓLOGA']
+      return formEntries.filter(i => [advogadaId, psicologaId].includes(i.widget_id))
+    } catch (e) {
+      return null
+    }
+  }
+
+  filterByEmail = (formEntries: FormEntry[]) => {
+    try {
+      const filteredEntries = formEntries.filter(i => i.fields[2].value === this.email)
+      if (filteredEntries.length > 0) {
+        return filteredEntries
+      }
+    } finally {
+      return null
+    }
+  }
+
+  start = async () => {
+    const formEntries = await this.getFormEntries()
+    if (!formEntries) {
+      throw new Error('getFormEntries error')
+    }
+    const scopedFormEntries = await this.filterByWidgetId(formEntries)
+    if (!scopedFormEntries) {
+      throw new Error('filterByWidgetId error')
+    }
+    const filteredFormEntries = await this.filterByEmail(scopedFormEntries)
+    if (!filteredFormEntries) {
+      throw new Error('filteredFormEntries error')
+    }
+
+    try {
+      return filteredFormEntries[0].created_at
+    } catch (e) {
+      console.log(e)
+      return new Date().toString()
+    }
+  }
+}
+
+export default BondeCreatedDate
