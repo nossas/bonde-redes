b97be48aec226a6014135eb8bf5fa974ac82a84d
Gabriel Rocha de Oliveira <rolivegab@localhost.localdomain>
2019-09-19 08:29:44 -0300

    feat(zendesk-ticket): add updateTicketRelation

---
+++ packages/cli-zendesk-ticket/src/updateTicketRelation.ts
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+const mutation = `mutation(
+  $id: Int
+  $webhooks_registry_id: Int
+) {
+  update_solidarity_zd_tickets(
+    where: {
+      id: {
+        _eq: $id
+      }
+    }
+    _set: {
+      webhooks_registry_id: $webhooks_registry_id
+    }
+  ) {
+    affected_rows
+  }
+}
+`
+
+const updateTicketRelation = async (id: number, webhooks_registry_id: number) => {
+  const { HASURA_API_URL, X_HASURA_ADMIN_SECRET } = process.env
+  const response = await axios.post(HASURA_API_URL, {
+    query: mutation,
+    variables: {
+      id,
+      webhooks_registry_id
+    }
+  }, {
+    headers: {
+      'x-hasura-admin-secret': X_HASURA_ADMIN_SECRET
+    }
+  })
+
+  return response
+}
+
+export default updateTicketRelation
