5a7ad533256c32a2580834bda521d1bc454887eb
Gabriel Rocha de Oliveira <rolivegab@localhost.localdomain>
2019-10-07 16:23:21 -0300

    feat(solidarity-match): implement saveMatches
    - This implementation only consider referrals maded in the last 30 days

--- packages/webhooks-solidarity-match/src/interfaces/IncomingRequestData.ts
+++ packages/webhooks-solidarity-match/src/interfaces/IncomingRequestData.ts
@@ -1,11 +1,9 @@
 import { Ticket } from "./Ticket";
 
 export interface IncomingRequestData {
-  payload: {
-    event: {
-      data: {
-        new: Ticket
-      }
+  event: {
+    data: {
+      new: Ticket
     }
   }
 }
