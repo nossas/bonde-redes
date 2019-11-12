e36d7246aa561768459e98cdd2340db3cd26e179
Gabriel Rocha de Oliveira <rolivegab@localhost.localdomain>
2019-11-12 17:33:59 -0300

    feat: implement new api

--- server/api.js
+++ server/api.js
@@ -1,85 +1,46 @@
 import parse from './parse'
-import getAllVolunteerUsers from './hasura/getAllVolunteerUsers'
-import getAllIndividualUsers from './hasura/getAllIndividualUsers'
-import getAllIndividualTickets from './hasura/getAllIndividualTickets'
-import getAllVolunteerTickets from './hasura/getAllVolunteerTickets'
+import getAllUsers from './hasura/getAllUsers'
+import getAllTickets from './hasura/getAllTickets'
 
-const dicio = {
-  therapist: 'Psicóloga',
-  lawyer: 'Advogada'
-}
-
-// export default main
 const main = async (req, res, next) => {
-  const {
-    serviceType, lat, lng, distance
-  } = req.query
-  // TODO: change filter
-  const services = ['therapist', 'lawyer', 'individual']
-
-  if (!services.includes(serviceType)) {
-    return res.status(400).json({ error: 'Query serviceType is invalid' })
+  const zendeskOrganizations = JSON.parse(process.env.REACT_APP_ZENDESK_ORGANIZATIONS)
+  const dicio = (organizationId) => {
+    const keys = Object.keys(zendeskOrganizations)
+    const key = keys.findIndex(i => zendeskOrganizations[i] === organizationId)
+    return ({
+      therapist: 'Psicóloga',
+      lawyer: 'Advogada'
+    })[keys[key]]
   }
+  const tickets = await getAllTickets()
+  const users = await getAllUsers()
 
-  const organizationId = {
-    therapist: 360282119532,
-    lawyer: 360269610652,
-    individual: 360273031591
-  }[serviceType]
-
-  if (['therapist', 'lawyer'].includes(serviceType)) {
-    const tickets = await getAllVolunteerTickets(organizationId)
-    const users = await getAllVolunteerUsers(organizationId, ['disponivel', 'aprovada'])
-    if (!users) {
-      return console.error('The API returned an error.')
-    }
-
-    const filteredUsers = users.map((user) => {
-      const filteredTickets = tickets.filter(ticket => ticket.requester_id === user.user_id && ticket.status !== 'deleted')
+  const usersWithTickets = users.map((user) => {
+    const filteredTickets = tickets.filter(ticket => ticket.requester_id === user.user_id && ticket.status !== 'deleted')
+    if ([zendeskOrganizations['therapist'], zendeskOrganizations['lawyer']].includes(user.organization_id)) {
       const ticket = filteredTickets.find(
-        i => i.subject === `[${dicio[serviceType]}] ${user.name} - ${user.registration_number}`
+        i => i.subject === `[${dicio(user.organizationId)}] ${user.name} - ${user.registration_number}`
       ) || filteredTickets.sort((a, b) => new Date(a.created_at) - new Date(b.created_at))[0]
+
       return {
         ...user,
         link_ticket: ticket && ticket.ticket_id,
         status_inscricao: ticket && ticket.status_inscricao
       }
-    })
-
-    const result = parse(filteredUsers, [lng, lat])
-      .filter(user => user.distance < Number(distance))
-      .sort((u1, u2) => u1.distance - u2.distance)
-
-    res.json(result)
-  } else {
-    const tickets = await getAllIndividualTickets(organizationId, 'solicitação_recebida')
-    const users = await getAllIndividualUsers(organizationId, 'inscrita')
-
-    if (!tickets || !users) {
-      return console.error('The API returned an error.')
-    }
-
-    const filteredUsers = users.filter((user) => {
-      const newUser = user
-      const filteredTickets = tickets.filter(ticket => ticket.requester_id === user.user_id && ticket.status !== 'deleted')
+    } else {
       const ticket = filteredTickets.sort((a, b) => new Date(a.created_at) - new Date(b.created_at))[0]
       if (!ticket) {
-        return false
+        return user
       }
-      newUser.link_ticket = ticket.ticket_id
-      newUser.status_acolhimento = ticket.status_acolhimento
+      user.link_ticket = ticket.ticket_id
+      user.status_acolhimento = ticket.status_acolhimento
       const tipo = ticket.subject.split(' ').slice(0, 1)[0]
-      newUser.tipo_acolhimento = ['[Psicológico]', '[Jurídico]'].includes(tipo) ? tipo.slice(1, -1) : ''
+      user.tipo_acolhimento = ['[Psicológico]', '[Jurídico]'].includes(tipo) ? tipo.slice(1, -1) : ''
+      return user
+    }
+  })
 
-      return filteredTickets.length > 0
-    })
-
-    const result = parse(filteredUsers, [lng, lat])
-      .filter(user => user.distance < Number(distance))
-      .sort((u1, u2) => u1.distance - u2.distance)
-
-    res.json(result)
-  }
+  res.json(usersWithTickets)
 }
 
 export default main
