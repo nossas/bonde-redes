b37e538bc2b43c59edabc7037a9750e62d1c3090
Gabriel Rocha de Oliveira <rolivegab@localhost.localdomain>
2019-11-21 17:21:51 -0300

    feat(registry): elastic apm

--- packages/webhooks-registry/env.d.ts
+++ packages/webhooks-registry/env.d.ts
@@ -5,5 +5,8 @@ declare namespace NodeJS {
     HASURA_API_URL: string
     HASURA_TABLE_NAME: string
     X_HASURA_ADMIN_SECRET: string
+    ELASTIC_APM_SERVICE_NAME: string
+    ELASTIC_APM_SECRET_TOKEN: string
+    ELASTIC_APM_SERVER_URL: string
   }
 }
