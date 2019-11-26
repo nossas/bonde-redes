e9bdaa04c25cf067a73085ec95d0e35bb64765bb
Gabriel Rocha de Oliveira <rolivegab@localhost.localdomain>
2019-11-26 09:48:35 -0300

    feat(count): new util libs

---
+++ packages/webhooks-solidarity-count/src/util/setCommunity.ts
@@ -0,0 +1,6 @@
+const setCommunity = <T extends object>(object: T) => {
+  const { COMMUNITY_ID } = process.env
+  return { ...object, community_id: Number(COMMUNITY_ID) }
+}
+
+export default setCommunity
