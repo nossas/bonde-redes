b2ff3f05a0ad5644f22086981abc816861ed28bc
Viviane <vivianedias@protonmail.com>
2019-12-09 19:15:44 -0300

    Remove table interface from match table context

--- src/context/match/table.ts
+++ src/context/match/table.ts
@@ -1,22 +1,5 @@
 import { createStateLink } from '@hookstate/core';
 
-interface MatchUsers {
-  latitude: string
-  longitude: string
-  name: string
-  email: string
-  data_de_inscricao_no_bonde?: string
-  status_inscricao: string
-  status_acolhimento: string
-  user_id: number
-  condition: string
-  organization_id: number
-  tipo_de_acholhimento: string
-  link_ticket: number
-}
-
-const tableDataRef = createStateLink<MatchUsers[]>([])
-
 interface Individual {
   email: string
   name: string
@@ -30,6 +13,5 @@ const individualRef = createStateLink<Individual>({
 })
 
 export default {
-  tableDataRef,
   individualRef
 }
