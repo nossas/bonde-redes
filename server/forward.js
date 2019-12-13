7e60b7a8faea80a425bea56c5862a0ee9d7f517f
Viviane <vivianedias@protonmail.com>
2019-12-13 09:29:11 -0300

    First individual ticket update done! YAY!

--- server/forward.js
+++ server/forward.js
@@ -17,6 +17,10 @@ const main = async (req, res, next) => {
   //   }
   //   res.json(JSON.stringify(result.verified, null, 2, true));
   // });
+  // client.tickets.show(req.body.individual_ticket_id, (err, req, result) => {
+  //   if (err) return handleError(err);
+  //   res.json(result, null, 2, true);
+  // });
   
   const client = zendesk.createClient({
     username:  ZENDESK_API_USER,
@@ -24,16 +28,154 @@ const main = async (req, res, next) => {
     remoteUri: ZENDESK_API_URL 
   });
   console.log({ ticketID: req.body.individual_ticket_id })
-  client.tickets.show(req.body.individual_ticket_id, (err, req, result) => {
+  var ticket = {
+    "ticket":
+      {
+        "status":"pending",
+      }
+  };
+  const ticket_id = req.body.individual_ticket_id
+  client.tickets.update(ticket_id, ticket, (err, req, result) => {
     if (err) return handleError(err);
     res.json(result, null, 2, true);
   });
-  
+
   const handleError = err => {
     console.log(err);
     process.exit(-1);
   }
-
 }
 
 export default main
+
+// {
+//   "url": "https://mapadoacolhimento.zendesk.com/api/v2/tickets/12586.json",
+//   "id": 12586,
+//   "external_id": "1231000",
+//   "via": {
+//       "channel": "api",
+//       "source": {
+//           "from": {},
+//           "to": {},
+//           "rel": null
+//       }
+//   },
+//   "created_at": "2019-08-07T21:37:06Z",
+//   "updated_at": "2019-09-24T21:49:20Z",
+//   "type": null,
+//   "subject": "[Jurídico] teste, Natal - RN",
+//   "raw_subject": "[Jurídico] teste, Natal - RN",
+//   "description": "Importado pelo BONDE.",
+//   "priority": null,
+//   "status": "new",
+//   "recipient": null,
+//   "requester_id": 385569543392,
+//   "submitter_id": 385569543392,
+//   "assignee_id": null,
+//   "organization_id": 360273031591,
+//   "group_id": null,
+//   "collaborator_ids": [],
+//   "follower_ids": [],
+//   "email_cc_ids": [],
+//   "forum_topic_id": null,
+//   "problem_id": null,
+//   "has_incidents": false,
+//   "is_public": true,
+//   "due_at": null,
+//   "tags": [
+//       "inscrita",
+//       "psicológico_e_jurídico",
+//       "rn",
+//       "solicitação_recebida"
+//   ],
+//   "custom_fields": [
+//       {
+//           "id": 360021879811,
+//           "value": "cidade6"
+//       },
+//       {
+//           "id": 360016631592,
+//           "value": ""
+//       },
+//       {
+//           "id": 360017432652,
+//           "value": null
+//       },
+//       {
+//           "id": 360016631632,
+//           "value": null
+//       },
+//       {
+//           "id": 360017056851,
+//           "value": "2019-08-07"
+//       },
+//       {
+//           "id": 360021665652,
+//           "value": null
+//       },
+//       {
+//           "id": 360014379412,
+//           "value": "solicitação_recebida"
+//       },
+//       {
+//           "id": 360021812712,
+//           "value": null
+//       },
+//       {
+//           "id": 360021879791,
+//           "value": null
+//       },
+//       {
+//           "id": 360016681971,
+//           "value": "teste"
+//       }
+//   ],
+//   "satisfaction_rating": null,
+//   "sharing_agreement_ids": [],
+//   "fields": [
+//       {
+//           "id": 360021879811,
+//           "value": "cidade6"
+//       },
+//       {
+//           "id": 360016631592,
+//           "value": ""
+//       },
+//       {
+//           "id": 360017432652,
+//           "value": null
+//       },
+//       {
+//           "id": 360016631632,
+//           "value": null
+//       },
+//       {
+//           "id": 360017056851,
+//           "value": "2019-08-07"
+//       },
+//       {
+//           "id": 360021665652,
+//           "value": null
+//       },
+//       {
+//           "id": 360014379412,
+//           "value": "solicitação_recebida"
+//       },
+//       {
+//           "id": 360021812712,
+//           "value": null
+//       },
+//       {
+//           "id": 360021879791,
+//           "value": null
+//       },
+//       {
+//           "id": 360016681971,
+//           "value": "teste"
+//       }
+//   ],
+//   "followup_ids": [],
+//   "brand_id": 360001789192,
+//   "allow_channelback": false,
+//   "allow_attachments": true
+// }
