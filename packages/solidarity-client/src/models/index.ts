6c31bac877c03592e0014470196c449f532e0d36
Viviane <vivianedias@protonmail.com>
2020-01-06 17:28:34 -0300

    feat(store): Integration for store and models

--- packages/solidarity-client/src/models/index.ts
+++ packages/solidarity-client/src/models/index.ts
@@ -2,9 +2,15 @@
 import volunteerModel from './volunteer'
 import agentModel from './agent'
 import popupModel from './popups'
+import tableModel from './table-data'
+import geobondeForm from './geobonde-form'
+import individualModel from './individual'
 
 export default {
   volunteerModel,
   agentModel,
-  popupModel
+  popupModel,
+  tableModel,
+  geobondeForm,
+  individualModel
 }
--- packages/solidarity-client/src/store.ts
+++ packages/solidarity-client/src/store.ts
@@ -1,24 +1,22 @@
 import { createStore } from 'easy-peasy';
 import GlobalModel from './models'
 
-const { volunteerModel, agentModel, popupModel } = GlobalModel
-
-const productsModel = {
-  items: {
-    1: { id: 1, name: 'Peas', price: 10 }
-  }
-};
-
-const basketModel = {
-  productIds: [1]
-};
+const {
+  volunteerModel,
+  agentModel,
+  popupModel,
+  geobondeForm,
+  tableModel,
+  individualModel
+} = GlobalModel
 
 const storeModel = {
-  products: productsModel,
-  basket: basketModel,
   volunteer: volunteerModel,
   agent: agentModel,
-  popups: popupModel
+  popups: popupModel,
+  table: tableModel,
+  geobonde: geobondeForm,
+  individual: individualModel
 };
 
 const store = createStore(storeModel);
