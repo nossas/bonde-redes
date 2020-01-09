05bfb46612a7c980d6ad521c021ab1b57a65e5f0
Viviane <vivianedias@protonmail.com>
2020-01-09 18:45:36 -0300

    fix(validation): Corrects backend validation
    Validation that was being made in the backend didnt work with ts, so the
    isValid func that checks if the req.body is valid, didnt work correctly.

--- packages/solidarity-client/server/forward.ts
+++ packages/solidarity-client/server/forward.ts
@@ -10,7 +10,7 @@ const {
   REACT_APP_ZENDESK_ORGANIZATIONS
 } = process.env
 
-const zendeskOrganizations = JSON.parse(REACT_APP_ZENDESK_ORGANIZATIONS || '')
+const zendeskOrganizations = JSON.parse(REACT_APP_ZENDESK_ORGANIZATIONS || '{}')
 const volunteerType = id => {
   if (id === zendeskOrganizations.lawyer) return 'Advogada'
   if (id === zendeskOrganizations.therapist) return 'Psicóloga'
@@ -124,17 +124,6 @@ const main = async (req, res, next) => {
     }
   };
 
-
-  // client.tickets.getComments(volunteer_ticket_id, (err, req, result) => {
-  //   if (err) return handleError(err);
-  //   res.json(result, null, 2, true);
-  // });
-
-  // client.tickets.show(volunteer_ticket_id, (err, req, result) => {
-  //   if (err) return handleError(err);
-  //   res.json(result, null, 2, true);
-  // });
-
   const handleError = ({ error, message }) => {
     console.log(error);
     res.json({ error, message })
@@ -177,140 +166,3 @@ const main = async (req, res, next) => {
 }
 
 export default main
-
-// {
-//   "url": "https://mapadoacolhimento.zendesk.com/api/v2/tickets/15617.json",
-//   "id": 15617,
-//   "external_id": null,
-//   "via": {
-//       "channel": "web",
-//       "source": {
-//           "from": {},
-//           "to": {},
-//           "rel": null
-//       }
-//   },
-//   "created_at": "2019-11-28T18:49:55Z",
-//   "updated_at": "2019-12-09T04:19:15Z",
-//   "type": null,
-//   "subject": "[Advogada] GABRIELA",
-//   "raw_subject": "[Advogada] GABRIELA",
-//   "description": "**EMAIL PARA A MSR**\nOlá, Irma!\n\nBoa notícia!\n\nConseguimos localizar uma advogada​ disponível próxima a você. Estamos te enviando os dados abaixo para que entre em contato em até 30 dias. É muito importante atentar-se a esse prazo pois, após esse período, a sua vaga irá expirar. Não se preocupe, caso você não consiga, poderá se cadastrar novamente no futuro.\n\nAdvogada​: GABRIELA\nTelefone: 48984782324​\nOAB: 49225\n\nTodos os atendimentos do Mapa devem ser gratuitos pelo tempo que durarem. Caso você seja cobrada, comunique imediatamente a nossa equipe. No momento de contato com a voluntária, por favor, identifique que você buscou ajuda via Mapa do Acolhimento.\n\nAgradecemos pela coragem, pela confiança e esperamos que seja bem acolhida! Pedimos que entre em contato para compartilhar a sua experiência de atendimento.\n\nUm abraço,\nAle, da equipe Mapa do Acolhimento",
-//   "priority": null,
-//   "status": "open",
-//   "recipient": null,
-//   "requester_id": 377567481991,
-//   "submitter_id": 377510044432,
-//   "assignee_id": 373018450472,
-//   "organization_id": 360269610652,
-//   "group_id": 360004360071,
-//   "collaborator_ids": [],
-//   "follower_ids": [],
-//   "email_cc_ids": [],
-//   "forum_topic_id": null,
-//   "problem_id": null,
-//   "has_incidents": false,
-//   "is_public": true,
-//   "due_at": null,
-//   "tags": [
-//       "1",
-//       "antiga",
-//       "branca",
-//       "disponivel",
-//       "encaminhamento__realizado",
-//       "jurídico",
-//       "mapa",
-//       "migracao-01",
-//       "sc"
-//   ],
-//   "custom_fields": [
-//       {
-//           "id": 360021879811,
-//           "value": null
-//       },
-//       {
-//           "id": 360016631592,
-//           "value": null
-//       },
-//       {
-//           "id": 360017432652,
-//           "value": "2019-11-28"
-//       },
-//       {
-//           "id": 360016631632,
-//           "value": "https://mapadoacolhimento.zendesk.com/agent/tickets/8757"
-//       },
-//       {
-//           "id": 360017056851,
-//           "value": null
-//       },
-//       {
-//           "id": 360021665652,
-//           "value": null
-//       },
-//       {
-//           "id": 360014379412,
-//           "value": "encaminhamento__realizado"
-//       },
-//       {
-//           "id": 360021812712,
-//           "value": null
-//       },
-//       {
-//           "id": 360021879791,
-//           "value": null
-//       },
-//       {
-//           "id": 360016681971,
-//           "value": "Irma"
-//       }
-//   ],
-//   "satisfaction_rating": null,
-//   "sharing_agreement_ids": [],
-//   "fields": [
-//       {
-//           "id": 360021879811,
-//           "value": null
-//       },
-//       {
-//           "id": 360016631592,
-//           "value": null
-//       },
-//       {
-//           "id": 360017432652,
-//           "value": "2019-11-28"
-//       },
-//       {
-//           "id": 360016631632,
-//           "value": "https://mapadoacolhimento.zendesk.com/agent/tickets/8757"
-//       },
-//       {
-//           "id": 360017056851,
-//           "value": null
-//       },
-//       {
-//           "id": 360021665652,
-//           "value": null
-//       },
-//       {
-//           "id": 360014379412,
-//           "value": "encaminhamento__realizado"
-//       },
-//       {
-//           "id": 360021812712,
-//           "value": null
-//       },
-//       {
-//           "id": 360021879791,
-//           "value": null
-//       },
-//       {
-//           "id": 360016681971,
-//           "value": "Irma"
-//       }
-//   ],
-//   "followup_ids": [],
-//   "brand_id": 360001789192,
-//   "allow_channelback": false,
-//   "allow_attachments": true
-// }
--- packages/solidarity-client/server/validator/forward.ts
+++ packages/solidarity-client/server/validator/forward.ts
@@ -3,19 +3,37 @@ import isEmpty from './is-empty'
 
 const checkField = input => !isEmpty(input) ? String(input) : ''
 
-const validate = data => {
-  const errors = {
-    agent: '',
-    individual_name: '',
-    individual_ticket_id: '',
-    volunteer_name: '',
-    volunteer_user_id: '',
-    volunteer_registry: '',
-    volunteer_phone: '',
-    volunteer_organization_id: ''
+const hasError = errors => {
+  const check = Object.values(errors).filter(i => i !== undefined)
+  console.log(check, check.length)
+  return check.length > 0
+}
 
+interface Errors {
+  agent: string | undefined
+  individual_name: string | undefined
+  individual_ticket_id: string | undefined
+  volunteer_name: string | undefined
+  volunteer_user_id: string | undefined
+  volunteer_registry: string | undefined
+  volunteer_phone: string | undefined
+  volunteer_organization_id: string | undefined
+}
+
+const validate = data => {
+  let errors: Errors = {
+    agent: undefined,
+    individual_name: undefined,
+    individual_ticket_id: undefined,
+    volunteer_name: undefined,
+    volunteer_user_id: undefined,
+    volunteer_registry: undefined,
+    volunteer_phone: undefined,
+    volunteer_organization_id: undefined
   }
+
   const zendeskOrganizations = JSON.parse(process.env.REACT_APP_ZENDESK_ORGANIZATIONS || '{}')
+
   const checkedData = {
     ...data,
     agent: checkField(data.agent),
@@ -30,65 +48,65 @@ const validate = data => {
 
   // Agent
   if (!Validator.isNumeric(checkedData.agent, { no_symbols: true })) {
-    errors.agent = 'ID do agente deve conter apenas números'
+    errors = { ...errors, agent: 'ID do agente deve conter apenas números' }
   }
   if (Validator.isEmpty(checkedData.agent)) {
-    errors.agent = 'Selecione um agente'
+    errors = { ...errors, agent: 'Selecione um agente' }
   }
 
   // Nome da MSR
   if (Validator.isEmpty(checkedData.individual_name)) {
-    errors.individual_name = 'O nome da MSR é obrigatório'
+    errors = { ...errors, individual_name: 'O nome da MSR é obrigatório' }
   }
 
   // Ticket ID da MSR
   if (!Validator.isNumeric(checkedData.individual_ticket_id, { no_symbols: true })) {
-    errors.individual_ticket_id = 'ID do ticket da MSR deve conter apenas números'
+    errors = { ...errors, individual_ticket_id: 'O nome da MSR é obrigatório' }
   }
   if (Validator.isEmpty(checkedData.individual_ticket_id)) {
-    errors.individual_ticket_id = 'Selecione uma MSR'
+    errors = { ...errors, individual_ticket_id: 'Selecione uma MSR' }
   }
 
   // Nome da Voluntária
   if (Validator.isEmpty(checkedData.volunteer_name)) {
-    errors.volunteer_name = 'O nome da voluntária é obrigatório'
+    errors = { ...errors, volunteer_name: 'O nome da voluntária é obrigatório' }
   }
 
   // User ID da Voluntária
   if (!Validator.isNumeric(checkedData.volunteer_user_id, { no_symbols: true })) {
-    errors.volunteer_user_id = 'user_id da voluntária deve conter apenas números'
+    errors = { ...errors, volunteer_user_id: 'user_id da voluntária deve conter apenas números' }
   }
   if (Validator.isEmpty(checkedData.volunteer_user_id)) {
-    errors.volunteer_user_id = 'Selecione uma Voluntária'
+    errors = { ...errors, volunteer_user_id: 'Selecione uma Voluntária' }
   }
 
   // Registro da Voluntária
   if (Validator.isEmpty(checkedData.volunteer_registry)) {
-    errors.volunteer_registry = 'A voluntária precisa ter um número de registro'
+    errors = { ...errors, volunteer_registry: 'A voluntária precisa ter um número de registro' }
   }
 
   // Telefone da Voluntária
   if (Validator.isEmpty(checkedData.volunteer_phone)) {
-    errors.volunteer_phone = 'A voluntária precisa ter um número de telefone'
+    errors = { ...errors, volunteer_phone: 'A voluntária precisa ter um número de telefone' }
   }
   if (!Validator.isNumeric(checkedData.volunteer_phone)) {
-    errors.volunteer_phone = 'Número da voluntária não é válido'
+    errors = { ...errors, volunteer_phone: 'Número da voluntária não é válido' }
   }
 
   // Organização da Voluntária
   if (Validator.isEmpty(checkedData.volunteer_organization_id)) {
-    errors.volunteer_organization_id = 'A voluntária precisa ter um organization_id'
+    errors = { ...errors, volunteer_organization_id: 'A voluntária precisa ter um organization_id' }
   }
   if (!Validator.isNumeric(checkedData.volunteer_organization_id, { no_symbols: true })) {
-    errors.volunteer_organization_id = 'organization_id da voluntária não contém apenas números'
+    errors = { ...errors, volunteer_organization_id: 'organization_id da voluntária não contém apenas números' }
   }
   if (Validator.equals(checkedData.volunteer_organization_id, zendeskOrganizations.individual)) {
-    errors.volunteer_organization_id = 'organization_id da voluntária não é válido'
+    errors = { ...errors, volunteer_organization_id: 'organization_id da voluntária não é válido' }
   }
-
+  console.log({errors, isValid: hasError(errors)})
   return {
     errors,
-    isValid: isEmpty(errors)
+    isValid: !hasError(errors)
   }
 }
 
