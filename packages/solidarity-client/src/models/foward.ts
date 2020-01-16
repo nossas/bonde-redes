6213676476405bdeff4de8668a7ee74a9f2cc5bd
Viviane <vivianedias@protonmail.com>
2020-01-16 14:00:05 -0300

    feat(comment): Add vol conditional type and registry
    Volunteer can be a therapist or lawyer. Depending on their
    organization_id the e-mail sent after the processes is different.

--- packages/solidarity-client/src/models/foward.ts
+++ packages/solidarity-client/src/models/foward.ts
@@ -26,6 +26,17 @@ const fowardModel = {
     } = payload
     try {
       console.log(data)
+      // const mockedBody = {
+      //   volunteer_name: "Ana Teste teste",
+      //   individual_name: "ANA MSR teste match automatizado",
+      //   individual_ticket_id: 16013,
+      //   agent: 373018450472,
+      //   volunteer_organization_id: 360269610652,
+      //   volunteer_registry: "99999",
+      //   volunteer_phone: "11999999999",
+      //   volunteer_user_id: 377577169651,
+      //   assignee_name: "Ana"
+      // }
       const response = await request.post(data)
       setSuccess(true)
       return response
