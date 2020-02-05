b0fa2e100b75e598d41f66411de7b605053b07fe
Viviane <vivianedias@protonmail.com>
2020-02-05 15:11:06 -0300

    feat(filter): Filter tickets by status
    Only count tickets that have a pending status.

--- packages/webhooks-solidarity-count/src/countTickets.ts
+++ packages/webhooks-solidarity-count/src/countTickets.ts
@@ -1,19 +1,23 @@
 import { TicketZendesk, customFields } from './interfaces/Ticket'
 
 const countTickets = (tickets: Array<TicketZendesk & customFields>) => {
-  const userCount = {
+  let userCount = {
     atendimentos_em_andamento_calculado_: 0,
     atendimentos_concludos_calculado_: 0,
     encaminhamentos_realizados_calculado_: 0,
   }
 
-  tickets.forEach((i) => {
-    if (i.status_acolhimento === 'atendimento__iniciado') {
-      userCount.atendimentos_em_andamento_calculado_ += 1
-    } else if (i.status_acolhimento === 'atendimento__concluído') {
-      userCount.atendimentos_concludos_calculado_ += 1
-    } else if (i.status_acolhimento === 'encaminhamento__realizado' || i.status_acolhimento === 'encaminhamento__realizado_para_serviço_público') {
-      userCount.encaminhamentos_realizados_calculado_ += 1
+
+  tickets.map((i) => {
+    if (i.status === 'pending') {
+      switch (i.status_acolhimento) {
+        case 'atendimento__iniciado':
+          return userCount.atendimentos_em_andamento_calculado_ += 1
+        case 'atendimento__concluído':
+          return userCount.atendimentos_concludos_calculado_ += 1
+        case 'encaminhamento__realizado':
+          return userCount.encaminhamentos_realizados_calculado_ += 1
+      }
     }
   })
 
