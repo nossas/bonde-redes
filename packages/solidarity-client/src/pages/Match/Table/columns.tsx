fffa4e9fb061b7b11f50bdd4879475fac5d8f90f
Viviane <vivianedias@protonmail.com>
2020-02-07 19:57:42 -0300

    feat: Match table colunt now shows ticket_created_at

--- packages/solidarity-client/src/pages/Match/Table/columns.tsx
+++ packages/solidarity-client/src/pages/Match/Table/columns.tsx
@@ -36,11 +36,11 @@ const columns = [
     Header: 'Tipo de acolhimento',
     Cell: ({ value }: valueArrayString) => (value ? (
       <span>{value}</span>
-    ) : null),
+    ) : '-'),
     width: 200
   }, {
-    accessor: 'data_de_inscricao_no_bonde',
-    Header: 'Data de inscrição no BONDE',
+    accessor: 'ticket_created_at',
+    Header: 'Data de criação do ticket',
     Cell: ({ value }: valueString) => {
       if (!value) {
         return '-'
