c4982dce091a32e44722c6e8ef6e42b946517ab9
Restyled.io <commits@restyled.io>
2020-02-18 16:46:22 +0000

    Restyled by clang-format

--- packages/solidarity-client/src/App.js
+++ packages/solidarity-client/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Route, Redirect } from 'react-router'
-import { Router } from 'react-router-dom'
-import styled from 'styled-components'
-import { StoreProvider } from 'easy-peasy';
-
 import 'mapbox-gl/dist/mapbox-gl.css'
-import { Footer } from 'bonde-styleguide'
-import history from './history'
-import store from './store'
+
+import {Footer} from 'bonde-styleguide'
+import {StoreProvider} from 'easy-peasy';
+import React from 'react'
+import {Redirect, Route} from 'react-router'
+import {Router} from 'react-router-dom'
+import styled from 'styled-components'
 
 import Header from './components/Header'
-import Geobonde from './pages/Geobonde/Table'
+import history from './history'
 import Map from './pages/Geobonde/Map'
+import Geobonde from './pages/Geobonde/Table'
 import Match from './pages/Match/Table'
 import VolunteersAvailable from './pages/VolunteersAvailable'
+import store from './store'
 
 const AppWrapper = styled.div`
   display: flex;
