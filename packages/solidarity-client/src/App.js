ec4b28f5cb2311bd8715b114eca42b9d635c2dfc
Restyled.io <commits@restyled.io>
2020-02-18 14:17:41 +0000

    Restyled by clang-format

--- packages/solidarity-client/src/App.js
+++ packages/solidarity-client/src/App.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { Route, Redirect } from 'react-router'
-import { Router } from 'react-router-dom'
-import styled from 'styled-components'
-import { StoreProvider } from 'easy-peasy';
-
 import 'mapbox-gl/dist/mapbox-gl.css'
-import { Footer } from 'bonde-styleguide'
-import history from './history'
-import store from './store'
+
+import {Footer} from 'bonde-styleguide'
+import {StoreProvider} from 'easy-peasy';
+import React from 'react'
+import {Redirect, Route} from 'react-router'
+import {Router} from 'react-router-dom'
+import styled from 'styled-components'
 
 import Header from './components/Header'
-import Geobonde from './pages/Geobonde/Table'
+import history from './history'
 import Map from './pages/Geobonde/Map'
+import Geobonde from './pages/Geobonde/Table'
 import Match from './pages/Match/Table'
-
-import { SessionProvider } from './services/session'
+import {SessionProvider} from './services/session'
+import store from './store'
 
 const AppWrapper = styled.div`
   display: flex;
@@ -31,30 +30,19 @@ const AppBody = styled.div`
 // `
 
 const App = () => (
-  <StoreProvider store={store}>
-    <Router history={history}>
-      <SessionProvider>
-        <AppWrapper className="app">
-          <Header />
-          <AppBody className="app-body">
-            <Route exact path="/">
-              <Redirect to="/geobonde" />
-            </Route>
-            <Route path="/geobonde" exact>
-              <Geobonde/>
-            </Route>
-            <Route path="/geobonde/map" exact>
-              <Map />
-            </Route>
-            <Route exact path="/match">
-              <Match />
-            </Route>
-          </AppBody>
-          <Footer />
-        </AppWrapper>
-      </SessionProvider>
-    </Router>
-  </StoreProvider>
-)
+    <StoreProvider store = {store}><Router history = {history}><SessionProvider>
+    <AppWrapper className = "app"><Header /><AppBody className = "app-body">
+    <Route exact path = "/"><Redirect to = "/geobonde" />
+    </Route>
+            <Route path="/geobonde " exact>
+     < Geobonde /></Route>
+            <Route path="/geobonde / map " exact>
+                   < Map /></Route>
+            <Route exact path="/match ">
+                            < Match /></Route>
+          </AppBody><Footer />
+    </AppWrapper>
+      </SessionProvider></Router>
+  </StoreProvider>)
 
 export default App
