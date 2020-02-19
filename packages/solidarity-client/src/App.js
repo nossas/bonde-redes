473b64ee8459eaf8204b44cf81a4360dee4f423a
Restyled.io <commits@restyled.io>
2020-02-19 20:07:50 +0000

    Restyled by prettier

--- packages/solidarity-client/src/App.js
+++ packages/solidarity-client/src/App.js
@@ -1,10 +1,10 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 
-import {Footer} from "bonde-styleguide";
-import {StoreProvider} from "easy-peasy";
+import { Footer } from "bonde-styleguide";
+import { StoreProvider } from "easy-peasy";
 import React from "react";
-import {Redirect, Route} from "react-router";
-import {Router} from "react-router-dom";
+import { Redirect, Route } from "react-router";
+import { Router } from "react-router-dom";
 import styled from "styled-components";
 
 import Header from "./components/Header";
@@ -13,7 +13,7 @@ import Map from "./pages/Geobonde/Map";
 import Geobonde from "./pages/Geobonde/Table";
 import Match from "./pages/Match/Table";
 import VolunteersAvailable from "./pages/VolunteersAvailable";
-import {SessionProvider} from "./services/session";
+import { SessionProvider } from "./services/session";
 import store from "./store";
 
 const AppWrapper = styled.div`
@@ -31,22 +31,33 @@ const AppBody = styled.div`
 // `
 
 const App = () => (
-    <StoreProvider store = {store}><Router history = {history}><SessionProvider>
-    <AppWrapper className = "app"><Header /><AppBody className = "app-body">
-    <Route exact path = "/"><Redirect to = "/match" />
-    </Route>
+  <StoreProvider store={store}>
+    <Router history={history}>
+      <SessionProvider>
+        <AppWrapper className="app">
+          <Header />
+          <AppBody className="app-body">
+            <Route exact path="/">
+              <Redirect to="/match" />
+            </Route>
             <Route exact path="/geobonde ">
-     < Geobonde /></Route>
+              <Geobonde />
+            </Route>
             <Route exact path="/geobonde / mapa ">
-                   < Map /></Route>
+              <Map />
+            </Route>
             <Route exact path="/match ">
-                            < Match />
-    </Route>
+              <Match />
+            </Route>
             <Route exact path="/voluntarias ">
-     < VolunteersAvailable /></Route>
-          </AppBody><Footer />
-    </AppWrapper>
-      </SessionProvider></Router>
-  </StoreProvider>);
+              <VolunteersAvailable />
+            </Route>
+          </AppBody>
+          <Footer />
+        </AppWrapper>
+      </SessionProvider>
+    </Router>
+  </StoreProvider>
+);
 
 export default App;
