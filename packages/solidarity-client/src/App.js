1ce1c992905f6fc0238256558c459e0850fb078e
Restyled.io <commits@restyled.io>
2020-02-20 15:07:15 +0000

    Restyled by prettier

--- packages/solidarity-client/src/App.js
+++ packages/solidarity-client/src/App.js
@@ -1,10 +1,10 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 
-import {Footer} from "bonde-styleguide";
-import {StoreProvider} from "easy-peasy";
+import { Footer } from "bonde-styleguide";
+import { StoreProvider } from "easy-peasy";
 import React from "react";
-import {Redirect, Route} from "react-router";
-import {Router} from "react-router-dom";
+import { Redirect, Route } from "react-router";
+import { Router } from "react-router-dom";
 import styled from "styled-components";
 
 import Header from "./components/Header";
@@ -13,7 +13,7 @@ import Map from "./pages/Geobonde/Map";
 import Geobonde from "./pages/Geobonde/Table";
 import Match from "./pages/Match/Table";
 import VolunteersAvailable from "./pages/VolunteersAvailable";
-import {SessionPageLayout, SessionProvider} from "./services/session";
+import { SessionPageLayout, SessionProvider } from "./services/session";
 import store from "./store";
 
 const AppWrapper = styled.div`
@@ -33,12 +33,10 @@ const AppBody = styled.div`
 const SamplePage = ({ children }) => (
   <AppWrapper className="app">
     <Header />
-    <AppBody className="app-body">
-      {children}
-    </AppBody>
+    <AppBody className="app-body">{children}</AppBody>
     <Footer />
   </AppWrapper>
-)
+);
 
 const App = () => (
   <SessionProvider>
@@ -49,8 +47,8 @@ const App = () => (
         </Route>
         <SessionPageLayout
           path="/geobonde"
-exact
-component = {() => (
+          exact
+          component={() => (
             <SamplePage>
               <Geobonde />
             </SamplePage>
