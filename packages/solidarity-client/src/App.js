1cfebaa2830bdaa56f00379cc976bdc2ffac035b
Restyled.io <commits@restyled.io>
2020-02-20 13:10:24 +0000

    Restyled by clang-format

--- packages/solidarity-client/src/App.js
+++ packages/solidarity-client/src/App.js
@@ -1,10 +1,10 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 
+import {Footer} from "bonde-styleguide";
+import {StoreProvider} from "easy-peasy";
 import React from "react";
-import { Footer } from "bonde-styleguide";
-import { StoreProvider } from "easy-peasy";
-import { Redirect, Route } from "react-router";
-import { Router } from "react-router-dom";
+import {Redirect, Route} from "react-router";
+import {Router} from "react-router-dom";
 import styled from "styled-components";
 
 import Header from "./components/Header";
@@ -13,9 +13,7 @@ import Map from "./pages/Geobonde/Map";
 import Geobonde from "./pages/Geobonde/Table";
 import Match from "./pages/Match/Table";
 import VolunteersAvailable from "./pages/VolunteersAvailable";
-
-import { SessionProvider } from "./services/session";
-
+import {SessionProvider} from "./services/session";
 import store from "./store";
 
 const AppWrapper = styled.div`
@@ -33,33 +31,22 @@ const AppBody = styled.div`
 // `
 
 const App = () => (
-  <StoreProvider store={store}>
-    <Router history={history}>
-      <SessionProvider>
-        <AppWrapper className="app">
-          <Header />
-          <AppBody className="app-body">
-            <Route exact path="/">
-              <Redirect to="/match" />
-            </Route>
-            <Route exact path="/geobonde">
-              <Geobonde />
-            </Route>
-            <Route exact path="/geobonde/mapa">
-              <Map />
-            </Route>
-            <Route exact path="/match">
-              <Match />
-            </Route>
-            <Route exact path="/voluntarias">
-              <VolunteersAvailable />
-            </Route>
-          </AppBody>
-          <Footer />
-        </AppWrapper>
-      </SessionProvider>
-    </Router>
-  </StoreProvider>
-);
+    <StoreProvider store = {store}><Router history = {history}><SessionProvider>
+    <AppWrapper className = "app"><Header /><AppBody className = "app-body">
+    <Route exact path = "/"><Redirect to = "/match" />
+    </Route>
+            <Route exact path="/geobonde ">
+     < Geobonde /></Route>
+            <Route exact path="/geobonde / mapa ">
+                   < Map /></Route>
+            <Route exact path="/match ">
+                            < Match />
+    </Route>
+            <Route exact path="/voluntarias ">
+     < VolunteersAvailable /></Route>
+          </AppBody><Footer />
+    </AppWrapper>
+      </SessionProvider></Router>
+  </StoreProvider>);
 
 export default App;
