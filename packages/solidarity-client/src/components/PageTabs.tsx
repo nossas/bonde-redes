f288bae975ee36e4e7c5ff72fae45d2c7e6349d0
Viviane <viviane@nossas.org>
2020-02-20 15:59:20 -0300

    feat: Add header tabs

---
+++ packages/solidarity-client/src/components/PageTabs.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Tab, TabItem } from 'bonde-styleguide'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Tabs = styled(Tab)`
+  position: absolute;
+  bottom: 0;
+`
+
+const PageTabs = ({ tabs, selectedTab }) => {
+  return (
+    <Tabs>
+      {tabs.map(({ name, to }, i) => (
+        <NavLink to={to}>
+          <TabItem
+            key={`page-tabs-${i}`}
+            active={selectedTab === name}
+          >
+            {name}
+          </TabItem>
+        </NavLink>
+      ))}
+  </Tabs>
+  )
+}
+
+export default PageTabs
