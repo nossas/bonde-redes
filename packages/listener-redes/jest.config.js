f226cc135627eabe66f6ed593b971cb4c0bc48a3
Restyled.io <commits@restyled.io>
2020-02-21 10:49:50 +0000

    Restyled by prettier

--- packages/listener-redes/jest.config.js
+++ packages/listener-redes/jest.config.js
@@ -1,18 +1,8 @@
 module.exports = {
-  testEnvironment : 'node',
-  transform : {"^.+\\.tsx?$" : "ts-jest"},
-  moduleFileExtensions : [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node",
-  ],
-  testRegex : '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
-  coverageDirectory : 'coverage',
-  collectCoverageFrom : [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ],
+  testEnvironment: 'node',
+  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
 };
