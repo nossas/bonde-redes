3d1b7a1a969d9b83757e8aa879d3ec9176dab1d0
Viviane <vivianedias@protonmail.com>
2020-02-21 10:21:17 -0300

    feat(relations): Loading matches in relations table

--- packages/solidarity-client/src/pages/Relations/index.tsx
+++ packages/solidarity-client/src/pages/Relations/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactTable from 'react-table'
 import styled from 'styled-components';
-
+import FetchMatches from '../../graphql/FetchMatches'
 import { Flexbox2 as Flexbox, Title } from 'bonde-styleguide'
 
 import 'react-table/react-table.css'
@@ -13,28 +13,31 @@ export const Wrap = styled.div`
 `
 
 const Table: React.FC = () => {
-  // TODO: getConnections
-  const tableData = []
-
-  return tableData.length === 0 ? (
-    <Flexbox middle>
-      <Wrap>
-        <Title.H4 margin={{ bottom: 30 }}>
-          Não existem conexões realizadas nessa comunidade.
-        </Title.H4>
-      </Wrap>
-    </Flexbox>
-  ) : (
-    <Flexbox middle>
-      <Wrap>
-        <ReactTable
-          data={tableData}
-          columns={columns}
-          defaultPageSize={10}
-          className="-striped -highlight"
-        />
-      </Wrap>
-    </Flexbox>
+  return (
+    <FetchMatches>
+      {(data) => {
+        return data.length === 0 ? (
+          <Flexbox middle>
+            <Wrap>
+              <Title.H4 margin={{ bottom: 30 }}>
+                Não existem conexões realizadas nessa comunidade.
+              </Title.H4>
+            </Wrap>
+          </Flexbox>
+        ) : (
+          <Flexbox middle>
+            <Wrap>
+              <ReactTable
+                data={data}
+                columns={columns}
+                defaultPageSize={10}
+                className="-striped -highlight"
+              />
+            </Wrap>
+          </Flexbox>
+        )
+    }}
+    </FetchMatches>
   )
 }
 
