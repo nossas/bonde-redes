8696c4a8b965f89a2cc3d1ac770d911539aa2042
Viviane <vivianedias@protonmail.com>
2020-03-04 17:09:44 -0300

    feat(logic): Centralize app logic in one func

--- packages/solidarity-client/src/components/BtnConnect.jsx
+++ packages/solidarity-client/src/components/BtnConnect.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { Button } from "bonde-styleguide";
-import { useStoreState, useStoreActions } from "easy-peasy";
-
-import { getUserData } from "../services/utils";
+import { useStoreActions } from "easy-peasy";
+import useAppLogic from '../app-logic'
 
 const BtnWarning = styled(Button)`
   border-color: ${props => (props.disabled ? "unset" : "#EE0090")}
@@ -16,8 +15,12 @@ const Connect = ({ id }) => {
     actions => actions.individual.setIndividual
   );
   const setPopup = useStoreActions(actions => actions.popups.setPopup);
-  const tableData = useStoreState(state => state.table.data);
-  const volunteer = useStoreState(state => state.volunteer.data);
+
+  const {
+    volunteer,
+    getUserData,
+    tableData,
+  } = useAppLogic()
 
   const onClick = () => {
     // TODO: Tratar caso em que a usuária não tem user_id
