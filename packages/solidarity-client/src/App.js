556fb210da656e358220e026078a199fe6e3ab4c
Restyled.io <commits@restyled.io>
2020-03-04 14:44:03 +0000

    Restyled by clang-format

--- packages/solidarity-client/src/App.js
+++ packages/solidarity-client/src/App.js
@@ -1,9 +1,9 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 
-import { StoreProvider } from "easy-peasy";
+import {StoreProvider} from "easy-peasy";
 import React from "react";
-import { Redirect, Route, Switch } from "react-router";
-import { Router } from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router";
+import {Router} from "react-router-dom";
 import styled from "styled-components";
 
 import Header from "./components/Header";
@@ -15,7 +15,7 @@ import GroupsWrapper from "./pages/Groups";
 import Relations from "./pages/Relations";
 // import FetchUsersByGroup from './graphql/FetchUsersByGroup'
 
-import { SessionPageLayout, SessionProvider } from "./services/session";
+import {SessionPageLayout, SessionProvider} from "./services/session";
 import store from "./store";
 
 const AppWrapper = styled.div`
@@ -30,16 +30,19 @@ const AppBody = styled.div`
 
 const InsideApp = () => (
   <AppWrapper>
-    <Header zIndex={0} />
+    <Header zIndex={
+  0} />
     <AppBody>
       <Switch>
         <Route exact path="/">
           <Redirect to="/groups" />
         </Route>
         <Route path="/groups" component={GroupsWrapper} />
-        <Route path="/geobonde " component={Geobonde} />
+        <Route path="/geobonde " component={
+  Geobonde} />
         <Route path="geobonde/mapa " component={Map} />
-        <Route path="/connect " component={Match} />
+        <Route path="/connect " component={
+  Match} />
         <Route path="/relations " component={Relations} />
       </Switch>
     </AppBody>
