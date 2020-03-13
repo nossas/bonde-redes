3a1fa39a2569fc69b751798706259800be3308dc
Viviane <vivianedias@protonmail.com>
2020-03-13 17:09:50 -0300

    feat(utils): Add type checking to utils

--- packages/client-redes/src/services/utils.ts
+++ packages/client-redes/src/services/utils.ts
@@ -1,17 +1,24 @@
-export const encodeText = input => encodeURIComponent(input);
+export const encodeText = (input: string): string => encodeURIComponent(input);
 
 export const whatsappText = ({
   volunteer_name,
   individual_name,
   agent,
   isVolunteer
-}) => {
+}): string => {
   if (isVolunteer)
     return `*${volunteer_name}, DEU MATCH* 🤝🏽 O seu número de atendimento acaba de ser enviado para a ${individual_name}! Esperamos que  ela entre em contato com você. Infelizmente, sabemos o quão difícil é dar esse passo e pode ser que ela não a procure. Ela possui 30 dias para entrar em contato, caso isso não aconteça dentro desse período, liberaremos a vaga para outra mulher. \n\n*Não se esqueça de nos avisar quando começar a atendê-la. Lembre que os atendimentos devem ser integralmente gratuitos.* \n\n Para te auxiliar, acesse a nossa cartilha com informações fundamentais sobre o acolhimento a mulheres em situação de violência. Acabamos de enviá-la por e-mail. \n\n Qualquer dúvida ou dificuldade, por favor nos comunique. \n Obrigada! É muito bom saber que podemos contar com você 💜\n Um abraço, \n ${agent} do Mapa do Acolhimento.`;
   return `*${volunteer_name}, DEU MATCH* 🤝🏽 O seu número de atendimento acaba de ser enviado para a ${individual_name}! Esperamos que  ela entre em contato com você. Infelizmente, sabemos o quão difícil é dar esse passo e pode ser que ela não a procure. Ela possui 30 dias para entrar em contato, caso isso não aconteça dentro desse período, liberaremos a vaga para outra mulher. \n\n*Não se esqueça de nos avisar quando começar a atendê-la. Lembre que os atendimentos devem ser integralmente gratuitos.* \n\n Para te auxiliar, acesse a nossa cartilha com informações fundamentais sobre o acolhimento a mulheres em situação de violência. Acabamos de enviá-la por e-mail. \n\n Qualquer dúvida ou dificuldade, por favor nos comunique. \n Obrigada! É muito bom saber que podemos contar com você 💜\n Um abraço, \n ${agent} do Mapa do Acolhimento.`;
 };
 
-export const parseNumber = input => input.replace(/\D/g, "");
+export const parseNumber = (input: string): string => input.replace(/\D/g, "");
 
-export const emailValidation = () =>
+export const emailValidation = (): RegExp =>
   /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+export const redirectToLogin = (): void => {
+  const loginUrl =
+    process.env.REACT_APP_LOGIN_URL ||
+    "http://admin-canary.bonde.devel:5002/auth/login";
+  window.location.href = `${loginUrl}?next=${window.location.href}`;
+};
