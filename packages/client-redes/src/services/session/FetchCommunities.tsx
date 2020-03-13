9bfe7fa5f798622ba7e091b63afd3fa7635f882e
Viviane <vivianedias@protonmail.com>
2020-03-13 17:23:43 -0300

    feat(communities): Add more type checks to FetchCommunities query component

--- packages/client-redes/src/services/session/FetchCommunities.tsx
+++ packages/client-redes/src/services/session/FetchCommunities.tsx
@@ -20,23 +20,51 @@ export interface Community {
   id: number;
   name: string;
   image: string;
+  created_at: string;
+  updated_at: string;
+  city: string;
 }
 
-export default ({ children, variables, defaultCommunity, onChange }: any) => {
+type FetchProps = {
+  children: (any) => React.ReactChildren;
+  variables: {
+    userId: number;
+  };
+  defaultCommunity: string;
+  onChange: (c: Community) => Promise<any>;
+};
+
+interface FetchRelatedCommunitiesData {
+  communities: Community[];
+}
+
+interface FetchRelatedCommunitiesVars {
+  userId: number;
+}
+
+export default ({
+  children,
+  variables,
+  defaultCommunity,
+  onChange
+}: FetchProps) => {
   const [community, setCommunity] = useState(defaultCommunity);
-  const { loading, error, data } = useQuery(FETCH_RELATED_COMMUNITIES, {
+  const { loading, error, data } = useQuery<
+    FetchRelatedCommunitiesData,
+    FetchRelatedCommunitiesVars
+  >(FETCH_RELATED_COMMUNITIES, {
     variables
   });
 
   if (loading) return <FullPageLoading message="Carregando comunidades..." />;
 
-  if (error || !data.communities) {
+  if (error || (data && !data.communities)) {
     console.log("error", { error, data });
     return children({ communities: [] });
   }
 
   const fetchCommunitiesProps = {
-    communities: data.communities,
+    communities: data && data.communities,
     community: Object.keys(community).length > 0 ? community : undefined,
     onChangeCommunity: c => {
       return onChange(c).then(() => setCommunity(c));
