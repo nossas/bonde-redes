a22771d1987ccd4907df78fbe6aec44ba0d3993d
Igor Santos <igr.exe@gmail.com>
2020-03-18 22:22:22 -0300

    feat(client): upgrade bonde-core-tools

--- packages/client-redes/src/App.tsx
+++ packages/client-redes/src/App.tsx
@@ -4,7 +4,6 @@ import { StoreProvider } from "easy-peasy";
 import React from "react";
 import { Redirect, Route, Switch } from "react-router";
 import { Router } from "react-router-dom";
-import styled from "styled-components";
 
 import Header from "./components/Header";
 import history from "./history";
@@ -12,26 +11,22 @@ import Match from "./pages/Connect";
 import GroupsWrapper from "./pages/Groups";
 import Relations from "./pages/Relations";
 
-import { SessionProvider, SessionPage } from "bonde-core-tools";
+import { BondeSessionProvider, BondeSessionUI } from "bonde-core-tools";
 import store from "./store";
 
-const AppWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const AppBody = styled.div`
-  min-height: 100vh;
-  flex-grow: 1;
-`;
+const config = {
+  loginUrl: process.env.REACT_APP_LOGIN_URL || 'http://admin-canary.bonde.devel:5002/auth/login',
+  graphqlApiUrl: process.env.REACT_APP_HASURA_API_URL || 'https://api-graphql.staging.bonde.org/v1/graphql',
+  crossStorageUrl: process.env.REACT_APP_DOMAIN_CROSS_STORAGE || 'http://cross-storage.bonde.devel'
+};
 
 const App = () => (
-  <SessionProvider baseLayout={SessionPage}>
+  <BondeSessionProvider config={config}>
     <StoreProvider store={store}>
       <Router history={history}>
-        <AppWrapper>
+        <BondeSessionUI.Main indexRoute='/'>
           <Header zIndex={0} />
-          <AppBody>
+          <BondeSessionUI.Content>
             <Switch>
               <Route exact path="/">
                 <Redirect to="/groups" />
@@ -40,11 +35,11 @@ const App = () => (
               <Route path="/connect" component={Match} />
               <Route path="/relations" component={Relations} />
             </Switch>
-          </AppBody>
-        </AppWrapper>
+          </BondeSessionUI.Content>
+        </BondeSessionUI.Main>
       </Router>
     </StoreProvider>
-  </SessionProvider>
+  </BondeSessionProvider>
 );
 
 export default App;
