b69cfbe624c7eefddfa6532de8614442dbaafce4
Viviane <vivianedias@protonmail.com>
2020-04-06 14:14:22 -0300

    feat(tabs): add tab routing to settings

--- packages/client-redes/src/components/Tabs.tsx
+++ packages/client-redes/src/components/Tabs.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import { Tab, TabItem } from "bonde-styleguide";
 import { NavLink } from "react-router-dom";
-import styled from "styled-components";
 
-const Tabs = styled(Tab)`
-  position: absolute;
-  bottom: 0;
-`;
-
-type PageTabs = {
+type Tabs = {
   tabs: Array<{ name: string; to: string }>;
   selectedTab: string;
 };
 
-const PageTabs = ({ tabs, selectedTab }: PageTabs) => {
+const Tabs = ({ tabs, selectedTab }: Tabs) => {
   return (
-    <Tabs>
+    <Tab>
       {tabs.map(({ name, to }, i) => (
         <NavLink to={to} key={`page-tabs-${i}`}>
           <TabItem active={selectedTab === name}>{name}</TabItem>
         </NavLink>
       ))}
-    </Tabs>
+    </Tab>
   );
 };
 
-export default PageTabs;
+export default Tabs;
