e9b707f9e000945902355df5f799eb910c588125
Viviane <vivianedias@protonmail.com>
2020-05-18 12:04:36 -0300

    improvement(tabs): add navigation through tabs

--- packages/solidarity-client/src/components/Header.tsx
+++ packages/solidarity-client/src/components/Header.tsx
@@ -1,81 +1,79 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import { Header as BondeHeader } from "bonde-styleguide";
 import { Header as Title } from "bonde-components";
 
-import Form from "./Form";
-import { If } from "./If";
-import MatchForm from "./MatchForm";
+import { Tabs, MatchForm, Form } from "../components";
 
 const StyledBondeHeader = styled(BondeHeader)`
   width: 100%;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-  display: flex;
   padding: 22px 40px;
-  align-items: center;
-`;
-
-const WrapButtons = styled.div`
   display: grid;
-  justify-items: end;
+  grid-template-columns: auto auto;
   align-items: center;
-  grid-template-columns: auto auto auto;
-  grid-column-gap: 20px;
-
-  & a {
-    font-family: inherit;
-    font-weight: 600;
-    font-size: 13px;
-    text-transform: uppercase;
-    text-decoration: none;
-    color: #fff;
-
-    &:hover,
-    &:active {
-      text-decoration: underline;
-      color: #ee0099;
-    }
-    &:focus {
-      color: #9b9b9b;
-    }
-  }
 `;
 
 const Wrap = styled.div`
   display: grid;
-  grid-template-rows: auto auto;
-  width: 100%;
   justify-items: end;
-  grid-row-gap: 20px;
+`;
+
+const TabsWrapper = styled.div`
+  position: relative;
+  top: 22px;
 `;
 
 const isMatch = (path: string) => path === "/match";
 
 const Header: React.FC = () => {
   const { pathname: path } = useLocation();
+  const [selectedTab, setTab] = useState("pessoas");
+
+  const tabs = [
+    {
+      name: "mapa",
+      to: "/geobonde/mapa"
+    },
+    {
+      name: "voluntárias",
+      to: "/voluntarias"
+    },
+    {
+      name: "encaminhamento",
+      to: "/match"
+    },
+    {
+      name: "geobonde",
+      to: "/geobonde"
+    }
+  ];
+
+  useEffect(() => {
+    const currentTab = tabs.find(i => i["to"] === path) || { name: "geobonde" };
+    setTab(currentTab["name"]);
+  }, [path, setTab, tabs]);
 
   return (
     <StyledBondeHeader>
-      <Title.h2 style={{ color: "#ffffff" }}>
+      <Title.h3 style={{ color: "#ffffff" }}>
         {isMatch(path) ? "Match" : "Mapa do acolhimento"}
-      </Title.h2>
+      </Title.h3>
       <Wrap>
-        <WrapButtons>
+        {/* <WrapButtons>
           {path !== "/geobonde/mapa" && <Link to="/geobonde/mapa">Mapa</Link>}
           {path !== "/voluntarias" && (
             <Link to="/voluntarias">Voluntárias</Link>
           )}
           {path !== "/match" && <Link to="/match">Encaminhamento</Link>}
           {path !== "/geobonde" && <Link to="/geobonde">Geobonde</Link>}
-        </WrapButtons>
-        <If condition={path === "/match"}>
-          <MatchForm />
-        </If>
-        <If condition={path === "/geobonde"}>
-          <Form />
-        </If>
+        </WrapButtons> */}
+        {path === "/match" && <MatchForm />}
+        {path === "/geobonde" && <Form />}
+        <TabsWrapper>
+          <Tabs tabs={tabs} selectedTab={selectedTab} />
+        </TabsWrapper>
       </Wrap>
     </StyledBondeHeader>
   );
--- packages/solidarity-client/src/components/index.ts
+++ packages/solidarity-client/src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Popup } from "./Popups/Popup";
 export { default as Loading } from "./Loading";
+export { default as Tabs } from "./Tabs";
+export { default as MatchForm } from "./MatchForm";
+export { default as Form } from "./Form";
