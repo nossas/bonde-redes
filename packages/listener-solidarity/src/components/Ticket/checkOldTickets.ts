87eee65416a430f851d3c0c94e5f2f1b6e78d19d
Viviane <vivianedias@protonmail.com>
2020-05-28 20:59:34 -0300

    feat(oldTickets): Closes old tickets if they're the same when user subscribes
    Adds logic to close tickets that have status new and have the same type as the new ones.

---
+++ packages/listener-solidarity/src/components/Ticket/checkOldTickets.ts
@@ -0,0 +1,62 @@
+import client from "../../zendesk";
+import dbg from "../../dbg";
+import { extractTypeFromSubject } from "../../utils";
+
+const log = dbg.extend("checkOldTickets");
+
+const updateTicketsStatus = (tickets) => {
+  log("Entering updateTicketsStatus");
+  return tickets.map((ticket, i: number) => {
+    const changeStatus = {
+      ticket: {
+        status: "closed" as "closed",
+        comment: {
+          body: "Fechado após MSR fazer um novo pedido de acolhimento identico",
+          public: false,
+        },
+      },
+    };
+    return setTimeout(() => {
+      return client.tickets.update(
+        ticket.id,
+        changeStatus,
+        (err, _req, result) => {
+          if (err && !result) {
+            log(
+              `Failed to update ticket status to closed, this is the ticket id '${ticket.id}'`
+            );
+            log(err);
+            return undefined;
+          }
+          log(
+            `${new Date()}: Ticket '${
+              ticket.id
+            }' status was susccessfully altered to closed`
+          );
+        }
+      );
+    }, i * 2500);
+  });
+};
+
+export default async ({ requester_id, subject }) => {
+  return client.tickets.listByUserRequested(
+    requester_id,
+    (err, _req, result: any) => {
+      if (err) {
+        log(`Failed to fetch tickets from user '${requester_id}'`);
+        log(err);
+        return undefined;
+      }
+      // Tickets com status novo e mesmo pedido de acolhimento
+      // são esses que não  devem ser gerados novamente
+      const newSubject = extractTypeFromSubject(subject);
+      const closeTickets = result.filter((oldTicket) => {
+        const oldSubject = extractTypeFromSubject(oldTicket.subject);
+        return oldSubject === newSubject && oldTicket.status === "new";
+      });
+      if (closeTickets.length > 0) return updateTicketsStatus(closeTickets);
+      return false;
+    }
+  );
+};
