01d911900829eb28c1ba006af5e18ee217d8b6df
Viviane <vivianedias@protonmail.com>
2020-06-03 19:47:03 -0300

    feat(oldTickets): Checks to see if user has old tickets that need to be closed
    This function checks if the user has old tickets that need to be closed or if the new ones will be created with status 'closed'.

--- packages/listener-solidarity/src/components/Ticket/checkOldTickets.ts
+++ packages/listener-solidarity/src/components/Ticket/checkOldTickets.ts
@@ -1,62 +1,28 @@
-import client from "../../zendesk";
 import { extractTypeFromSubject } from "../../utils";
+import { Ticket } from "../../types";
 import dbg from "../../dbg";
 
 const log = dbg.extend("checkOldTickets");
 
-const updateTicketsStatus = tickets => {
-  log("Entering updateTicketsStatus");
-  return tickets.map((ticket, i: number) => {
-    const changeStatus = {
-      ticket: {
-        status: "closed" as "closed",
-        comment: {
-          body: "Fechado após MSR fazer um novo pedido de acolhimento identico",
-          public: false
-        }
-      }
-    };
-    return setTimeout(() => {
-      return client.tickets.update(
-        ticket.id,
-        changeStatus,
-        (err, _req, result) => {
-          if (err && !result) {
-            log(
-              `Failed to update ticket status to closed, this is the ticket id '${ticket.id}'`
-            );
-            log(err);
-            return undefined;
-          }
-          log(
-            `${new Date()}: Ticket '${
-              ticket.id
-            }' status was susccessfully altered to closed`
-          );
-        }
-      );
-    }, i * 2500);
+export default async (subject: string, tickets: Ticket[]) => {
+  log("Checking old tickets");
+  const newSubject = extractTypeFromSubject(subject);
+  const hasSameSubject = tickets.filter((oldTicket) => {
+    const oldSubject = extractTypeFromSubject(oldTicket.subject);
+    return oldSubject === newSubject;
   });
-};
 
-export default async ({ requester_id, subject }) => {
-  return client.tickets.listByUserRequested(
-    requester_id,
-    (err, _req, result: any) => {
-      if (err) {
-        log(`Failed to fetch tickets from user '${requester_id}'`);
-        log(err);
-        return undefined;
-      }
-      // Tickets com status novo e mesmo pedido de acolhimento
-      // são esses que não  devem ser gerados novamente
-      const newSubject = extractTypeFromSubject(subject);
-      const closeTickets = result.filter(oldTicket => {
-        const oldSubject = extractTypeFromSubject(oldTicket.subject);
-        return oldSubject === newSubject && oldTicket.status === "new";
-      });
-      if (closeTickets.length > 0) return updateTicketsStatus(closeTickets);
-      return false;
-    }
+  if (hasSameSubject.length < 1) return undefined;
+
+  const hasNewTickets = hasSameSubject.filter(
+    (oldTicket) => oldTicket.status === "new"
   );
+  if (hasNewTickets.length > 0) return hasNewTickets;
+
+  const hasMatch = hasSameSubject.filter(
+    (oldTicket) => oldTicket.status === "open" || oldTicket.status === "pending"
+  );
+  if (hasMatch.length > 0) return "hasMatch";
+
+  return undefined;
 };
