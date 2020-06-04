7265b6c30416083ad59c0786e58c5b56ebbaf47b
Viviane <vivianedias@protonmail.com>
2020-06-04 17:56:19 -0300

    feat(tickets): update checkOldTickets logic to latest

--- packages/listener-solidarity/src/components/Ticket/checkOldTickets.ts
+++ packages/listener-solidarity/src/components/Ticket/checkOldTickets.ts
@@ -4,7 +4,7 @@ import dbg from "../../dbg";
 
 const log = dbg.extend("checkOldTickets");
 
-export default async (subject: string, tickets: Ticket[]) => {
+export default (subject: string, tickets: Ticket[]) => {
   log("Checking old tickets");
   const newSubject = extractTypeFromSubject(subject);
   const hasSameSubject = tickets.filter((oldTicket) => {
@@ -14,15 +14,34 @@ export default async (subject: string, tickets: Ticket[]) => {
 
   if (hasSameSubject.length < 1) return undefined;
 
-  const hasNewTickets = hasSameSubject.filter(
-    (oldTicket) => oldTicket.status === "new"
-  );
-  if (hasNewTickets.length > 0) return hasNewTickets;
+  // "atendimento__concluído"
+  // "atendimento__iniciado"
+  // "atendimento__interrompido"
+  // "encaminhamento__aguardando_confirmação"
+  // "encaminhamento__confirmou_disponibilidade"
+  // "encaminhamento__negado"
+  // "encaminhamento__realizado"
+  // "encaminhamento__realizado_para_serviço_público"
+  // "solicitação_recebida";
 
-  const hasMatch = hasSameSubject.filter(
-    (oldTicket) => oldTicket.status === "open" || oldTicket.status === "pending"
-  );
-  if (hasMatch.length > 0) return "hasMatch";
+  const hasOldTickets = hasSameSubject.filter((oldTicket) => {
+    const status_acolhimento = oldTicket.fields.find(
+      (field) => field.id === 360014379412
+    );
+    if (status_acolhimento && status_acolhimento.value) {
+      return (
+        (status_acolhimento.value === "solicitação_recebida" ||
+          status_acolhimento.value === "atendimento__iniciado" ||
+          status_acolhimento.value === "encaminhamento__realizado" ||
+          status_acolhimento.value ===
+            "encaminhamento__realizado_para_serviço_público") &&
+        oldTicket.status !== "closed" &&
+        oldTicket.status !== "solved"
+      );
+    }
+    return false;
+  });
 
+  if (hasOldTickets.length > 0) return true;
   return undefined;
 };
