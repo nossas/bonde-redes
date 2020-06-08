bcf7fb646616a1e5ab6ae94d47b5ad02a1ce7475
Viviane <vivianedias@protonmail.com>
2020-06-08 17:56:41 -0300

    feat(mautic): renames package webhooks-zendesk to webhooks-mautic-zendesk
    the name replacement was done so webhook function was more self-explanatory.

---
+++ packages/webhooks-mautic-zendesk/src/Server.ts
@@ -0,0 +1,365 @@
+import Express, { Response } from "express";
+import debug, { Debugger } from "debug";
+import AdvogadaCreateUser from "./integrations/AdvogadaCreateUser";
+import PsicologaCreateUser from "./integrations/PsicologaCreateUser";
+import ListTicketsFromUser from "./integrations/ListTicket";
+import AdvogadaCreateTicket from "./integrations/AdvogadaCreateTicket";
+import AdvogadaUpdateTicket from "./integrations/AdvogadaUpdateTicket";
+import PsicologaCreateTicket from "./integrations/PsicologaCreateTicket";
+import PsicologaUpdateTicket from "./integrations/PsicologaUpdateTicket";
+import { FILTER_SERVICE_STATUS, filterService } from "./filterService";
+import { FILTER_FORM_NAME_STATUS, filterFormName } from "./filterFormName";
+import BondeCreatedDate from "./integrations/BondeCreatedDate";
+import addTagsToTicket from "./zendesk/addTagsToTicket";
+
+interface DataType {
+  data: {
+    logTable: {
+      returning: {
+        id: number;
+      }[];
+    };
+  };
+}
+
+interface FormData {
+  cep: string;
+}
+
+class Server {
+  private server = Express().use(Express.json());
+
+  private dbg: Debugger;
+
+  private formData?: FormData;
+
+  constructor() {
+    this.dbg = debug("webhooks-mautic-zendesk");
+  }
+
+  dictionary: { [s: string]: string } = {
+    aprovada: "aprovada",
+    reprovada_estudo_de_caso: "reprovada_-_estudo_de_caso",
+    reprovada_registro_inválido: "reprovada_-_registro_inválido",
+    reprovada_diretrizes_do_mapa: "reprovada_-_diretrizes_do_mapa"
+  };
+
+  createTicket = async (
+    instance: AdvogadaCreateUser | PsicologaCreateUser,
+    {
+      id,
+      organization_id,
+      name,
+      phone,
+      user_fields: { registration_number, condition, state, city }
+    },
+    created_at: string,
+    res: Response
+  ) => {
+    const listTickets = new ListTicketsFromUser(id, res);
+    const tickets = await listTickets.start();
+    if (!tickets) {
+      return undefined;
+    }
+    const filteredTickets = tickets.data.tickets.filter((i: any) => {
+      if (instance instanceof AdvogadaCreateUser) {
+        return (
+          ["open", "new", "pending", "hold"].includes(i.status) &&
+          i.subject === `[Advogada] ${name} - ${registration_number}`
+        );
+      }
+      return (
+        ["open", "new", "pending", "hold"].includes(i.status) &&
+        i.subject === `[Psicóloga] ${name} - ${registration_number}`
+      );
+    });
+
+    if (filteredTickets.length === 0) {
+      if (instance instanceof AdvogadaCreateUser) {
+        const advogadaCreateTicket = new AdvogadaCreateTicket(res);
+        return advogadaCreateTicket.start<any>({
+          requester_id: id,
+          organization_id,
+          description: "-",
+          status_inscricao: "aprovada",
+          subject: `[Advogada] ${name} - ${registration_number}`,
+          custom_fields: [
+            {
+              id: 360021665652,
+              value: this.dictionary[condition]
+            },
+            {
+              id: 360016631592,
+              value: name
+            },
+            {
+              id: 360021812712,
+              value: phone
+            },
+            {
+              id: 360021879791,
+              value: state
+            },
+            {
+              id: 360021879811,
+              value: city
+            }
+          ],
+          created_at
+        });
+      }
+      const psicólogaCreateTicket = new PsicologaCreateTicket(res);
+      return psicólogaCreateTicket.start<any>({
+        requester_id: id,
+        organization_id,
+        description: "-",
+        status_inscricao: "aprovada",
+        subject: `[Psicóloga] ${name} - ${registration_number}`,
+        custom_fields: [
+          {
+            id: 360021665652,
+            value: this.dictionary[condition]
+          },
+          {
+            id: 360016631592,
+            value: name
+          },
+          {
+            id: 360021812712,
+            value: phone
+          },
+          {
+            id: 360021879791,
+            value: state
+          },
+          {
+            id: 360021879811,
+            value: city
+          }
+        ],
+        created_at
+      });
+    }
+    if (instance instanceof AdvogadaCreateUser) {
+      const advogadaUpdateTicket = new AdvogadaUpdateTicket(
+        filteredTickets[0].id,
+        res
+      );
+      return advogadaUpdateTicket.start<any>({
+        requester_id: id,
+        organization_id,
+        description: "-",
+        status_inscricao: "aprovada",
+        subject: `[Advogada] ${name} - ${registration_number}`,
+        custom_fields: [
+          {
+            id: 360021665652,
+            value: this.dictionary[condition]
+          },
+          {
+            id: 360016631592,
+            value: name
+          },
+          {
+            id: 360021812712,
+            value: phone
+          },
+          {
+            id: 360021879791,
+            value: state
+          },
+          {
+            id: 360021879811,
+            value: city
+          }
+        ]
+      });
+    }
+    const psicólogaUpdateTicket = new PsicologaUpdateTicket(
+      filteredTickets[0].id,
+      res
+    );
+    return psicólogaUpdateTicket.start<any>({
+      requester_id: id,
+      organization_id,
+      description: "-",
+      status_inscricao: "aprovada",
+      subject: `[Psicóloga] ${name} - ${registration_number}`,
+      custom_fields: [
+        {
+          id: 360021665652,
+          value: this.dictionary[condition]
+        },
+        {
+          id: 360016631592,
+          value: name
+        },
+        {
+          id: 360021812712,
+          value: phone
+        },
+        {
+          id: 360021879791,
+          value: state
+        },
+        {
+          id: 360021879811,
+          value: city
+        }
+      ]
+    });
+  };
+
+  checkNames = ({ primeiro_nome, sobrenome_completo }: any) => {
+    let aux = "";
+    if (typeof primeiro_nome === "string" && primeiro_nome.length > 0) {
+      aux += primeiro_nome;
+    }
+
+    if (
+      typeof sobrenome_completo === "string" &&
+      sobrenome_completo.length > 0
+    ) {
+      aux += ` ${sobrenome_completo}`;
+    }
+
+    if (aux.length > 0) {
+      return aux;
+    }
+
+    return null;
+  };
+
+  checkCep = (cep: any) => {
+    if (typeof cep === "string" && cep.length > 0) {
+      return cep;
+    }
+
+    return null;
+  };
+
+  start = () => {
+    const { PORT } = process.env;
+    this.server
+      .post("/", async (req, res) => {
+        const {
+          status: serviceStatus,
+          serviceName,
+          data
+        } = await filterService(req.body);
+
+        if (serviceStatus === FILTER_SERVICE_STATUS.NOT_DESIRED_SERVICE) {
+          return res
+            .status(200)
+            .json(
+              `Service "${serviceName}" isn't desired, but everything is OK.`
+            );
+        }
+        if (serviceStatus === FILTER_SERVICE_STATUS.INVALID_REQUEST) {
+          this.dbg("Erro desconhecido ao filtrar por serviço.");
+          return res
+            .status(400)
+            .json("Erro desconhecido ao filtrar por serviço.");
+        }
+
+        const {
+          InstanceClass,
+          results,
+          status: formNameStatus,
+          name,
+          data: errorData,
+          dateSubmitted
+        } = await filterFormName(data!);
+        if (formNameStatus === FILTER_FORM_NAME_STATUS.FORM_NOT_IMPLEMENTED) {
+          this.dbg(`Form "${name}" not implemented. But it's ok`);
+          return res
+            .status(200)
+            .json(`Form "${name}" not implemented. But it's ok`);
+        }
+        if (formNameStatus === FILTER_FORM_NAME_STATUS.INVALID_REQUEST) {
+          this.dbg("Invalid request.");
+          this.dbg(errorData);
+          return res.status(400).json("Invalid request, see logs.");
+        }
+
+        if (!results || !dateSubmitted) {
+          return res
+            .status(400)
+            .json("Invalid request, failed to parse results");
+        }
+        const bondeCreatedDate = new BondeCreatedDate(
+          results.email,
+          this.checkNames(results),
+          this.checkCep(results.cep)
+        );
+        const bondeCreatedAt = await bondeCreatedDate.start();
+
+        if (!bondeCreatedAt) {
+          return this.dbg(bondeCreatedAt);
+        }
+
+        const instance = await new InstanceClass!(res);
+        let user;
+        if (instance instanceof AdvogadaCreateUser) {
+          user = await instance.start(results, bondeCreatedAt);
+        } else if (instance instanceof PsicologaCreateUser) {
+          user = await instance.start(results!, bondeCreatedAt);
+        }
+
+        if (!user.response) {
+          this.dbg("Failed to create user");
+          return res.status(500).json("Failed to create user");
+        }
+
+        const {
+          response: {
+            data: {
+              user: createdUser,
+              user: {
+                created_at: responseCreatedAt,
+                updated_at: responseUpdatedAt,
+                id: userId
+              }
+            }
+          },
+          tags
+        } = user;
+        if (responseCreatedAt === responseUpdatedAt) {
+          this.dbg(`Success, created user "${userId}"!`);
+        } else {
+          this.dbg(`Success, updated user "${userId}"!`);
+        }
+
+        const resultTicket = await this.createTicket(
+          instance,
+          createdUser,
+          dateSubmitted,
+          res
+        );
+        if (resultTicket) {
+          this.dbg(`Success updated ticket "${resultTicket.data.ticket.id}".`);
+
+          if (tags) {
+            const response = await addTagsToTicket(
+              resultTicket.data.ticket.id,
+              tags
+            );
+            if (response && response.status === 200) {
+              this.dbg(
+                `Success add tag "${tags}" to ticket "${resultTicket.data.ticket.id}".`
+              );
+            }
+          }
+
+          return res.status(200).json("Success finish integration");
+        }
+        this.dbg("Failed to create ticket");
+        return res.status(500).json("Failed failed integration");
+      })
+      .listen(Number(PORT), "0.0.0.0", () => {
+        this.dbg(`Server listen on port ${PORT}`);
+      });
+  };
+}
+
+export default Server;
---
+++ packages/webhooks-mautic-zendesk/src/integrations/Base.ts
@@ -0,0 +1,164 @@
+import debug, { Debugger } from "debug";
+import urljoin from "url-join";
+import axios from "axios";
+import { Response } from "express";
+
+export enum GMAPS_ERRORS {
+  REQUEST_FAILED,
+  INVALID_INPUT
+}
+
+abstract class Base {
+  protected name: string;
+
+  protected dbg: Debugger;
+
+  protected url: string;
+
+  protected organizations: { [s: string]: number };
+
+  protected res: Response;
+
+  private method: "GET" | "POST" | "PUT";
+
+  constructor(
+    name: string,
+    url: string,
+    res: Response,
+    method: "GET" | "POST" | "PUT" = "POST"
+  ) {
+    this.method = method;
+    this.name = `webhooks-mautic-zendesk:${name}`;
+    this.dbg = debug(this.name);
+    this.url = url;
+    this.res = res;
+    const { ZENDESK_ORGANIZATIONS } = process.env;
+    this.organizations = JSON.parse(ZENDESK_ORGANIZATIONS);
+  }
+
+  protected getAddress = async (cep: string) => {
+    const log = debug("gmaps");
+    const { GOOGLE_MAPS_API_KEY } = process.env;
+    let data;
+    try {
+      log(`requesting google with cep ${cep}...`);
+      const response = await axios.post(
+        "https://maps.googleapis.com/maps/api/geocode/json",
+        undefined,
+        {
+          params: {
+            address: cep,
+            key: GOOGLE_MAPS_API_KEY
+          }
+        }
+      );
+      log("response!", response.data);
+      data = response.data;
+    } catch (e) {
+      this.dbg("falha na requisição para o google maps");
+      return {
+        error: GMAPS_ERRORS.REQUEST_FAILED
+      };
+    }
+
+    if (data.status === "OK") {
+      const {
+        results: [
+          {
+            geometry: {
+              location: { lat, lng }
+            },
+            address_components: addressComponents,
+            formatted_address: address
+          }
+        ]
+      } = data;
+
+      let state: string | undefined;
+      let city: string | undefined;
+      let country: string | undefined;
+      let tagInvalidCep = false;
+
+      addressComponents.forEach(
+        ({
+          types,
+          short_name: shortName
+        }: {
+          types: string[];
+          short_name: string;
+        }) => {
+          if (types.includes("administrative_area_level_1")) {
+            state = shortName;
+          }
+          if (types.includes("administrative_area_level_2")) {
+            city = shortName;
+          }
+          if (types.includes("country")) {
+            country = shortName;
+          }
+        }
+      );
+
+      if (country !== "BR") {
+        state = undefined;
+        city = undefined;
+        tagInvalidCep = true;
+      }
+
+      return {
+        lat,
+        lng,
+        address,
+        state,
+        city,
+        tagInvalidCep
+      };
+    }
+
+    return {
+      error: GMAPS_ERRORS.INVALID_INPUT
+    };
+  };
+
+  protected send = async <T = any>(data?: any) => {
+    const {
+      ZENDESK_API_URL,
+      ZENDESK_API_TOKEN,
+      ZENDESK_API_USER
+    } = process.env;
+    const endpoint = urljoin(ZENDESK_API_URL!, this.url);
+    try {
+      if (this.method === "POST") {
+        const result = await axios.post<T>(endpoint, data, {
+          auth: {
+            username: ZENDESK_API_USER,
+            password: ZENDESK_API_TOKEN
+          }
+        });
+        return result;
+      }
+      if (this.method === "GET") {
+        const result = await axios.get<T>(endpoint, {
+          auth: {
+            username: ZENDESK_API_USER,
+            password: ZENDESK_API_TOKEN
+          }
+        });
+        return result;
+      }
+      const result = await axios.put<T>(endpoint, data, {
+        auth: {
+          username: ZENDESK_API_USER,
+          password: ZENDESK_API_TOKEN
+        }
+      });
+      return result;
+    } catch (e) {
+      return this.dbg(e);
+    }
+  };
+
+  abstract start: Function;
+}
+
+export default Base;
